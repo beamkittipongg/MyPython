c9fa6059448d30f891049e6e3dc8f91ed48d2900
ning <6629011017@cdti.ac.th>
2024-06-24 14:42:52 +0700

    update

---
+++ lab4-3.py
@@ -0,0 +1,9 @@
+person = {"name": "Kittipong", "age":17, "wt":95, "h":172}
+#for n in person
+#    print(n)
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"]="Kittipong"
+print(person["name"])
